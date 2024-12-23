5d3701f7d55c9f6c7259be19e923a924a649e739
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2024-12-23 16:51:01 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2N8UnNPB3Y7fhtz1I4npAzTEit8b-FkU",
+  authDomain: "auth-moha-milon-f98b3.firebaseapp.com",
+  projectId: "auth-moha-milon-f98b3",
+  storageBucket: "auth-moha-milon-f98b3.firebasestorage.app",
+  messagingSenderId: "946415909340",
+  appId: "1:946415909340:web:d6f8bfa8c6b95f3c9d7719"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './component/Root.jsx';
+import Home from './component/Home.jsx';
+import Login from './component/Login.jsx';
+import Resister from './component/Resister.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'resister',
+        element: <Resister></Resister>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
